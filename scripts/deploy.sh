57eb7a4aca2be4b1bc90f0b02e6e97f3e2fcafbd
Marco <marco.egbring@epha.ch>
2019-08-26 14:29:30 +0200

    Upload Fresh Build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,9 +8,11 @@ echo "-------------------------------------"
 echo " Upload Fresh Showcase"
 echo "-------------------------------------"
 echo ""
+view="showcase"
 
 if [[ $1 == "staging" ]]
 then
+  dist="E3O8ET4HHUEUOM"
 
   npm update
 
@@ -22,15 +24,14 @@ then
   git push && git push --tags
 
 
-  aws --region eu-central-1 s3 --delete sync dist s3://epha.io/showcase/ --exclude .DS_Store
+  aws --region eu-central-1 s3 --delete sync dist s3://epha.io/$view/ --exclude .DS_Store
 
   echo ""
-  did=E3O8ET4HHUEUOM
-  pth="/showcase/index.html"
-  echo "Invalidating index.html"
-  iid=$(aws cloudfront create-invalidation --distribution-id $did --paths $pth | grep Id | awk -F '"' '{print $4}')
-  # aws cloudfront invalidation-completed --distribution-id $did --id $iid && notfiy "Invalidation complete"
-
+  paths="/$view/index.html"
+  echo "Invalidating ${paths}"
+  id=$(aws cloudfront create-invalidation --distribution-id $dist --paths $paths | grep Id | awk -F '"' '{print $4}')
+  aws cloudfront wait invalidation-completed --distribution-id $dist --id $id
+  osascript -e 'display notification "Invalidated $view!" with title "AWS" subtitle "Cloudfront" sound name "Submarine"'
 fi
 
 printf " \n"
