635265c1f448d7ce96c2d5d09448a8143042e6ef
Marco <marco.egbring@epha.ch>
2019-09-02 11:15:04 +0200

    Upload Fresh Build

--- showcase/views/literature.js
+++ showcase/views/literature.js
@@ -13,8 +13,11 @@ export function literature( state ) {
 
       <h1>Element unit-literature</h1>
 
-      <h2>Example</h2>
-      <unit-literature @action=${onAction} .state=${{ auto:false, items:example }}></unit-literature>
+      <h2>Example Theme nice (default)</h2>
+      <unit-literature style="border:1px solid tomato;" @action=${onAction} .state=${{ auto:false, items:example }}></unit-literature>
+
+      <h2>Example Theme pubmed</h2>
+      <unit-literature style="border:1px solid tomato;" @action=${onAction} theme="pubmed" .state=${{ auto:false, items:example }}></unit-literature>
 
       <h2>Signature</h2>
       <unit-md .state=${{ raw: unitLiterature.signature, theme:"tomorrow" }}></unit-md>
