791b61500a05690ed09be4245eb105687d41a7ea
Marco <marco.egbring@epha.ch>
2019-09-03 11:22:47 +0200

    Upload Fresh Build

--- showcase/views/textarea.js
+++ showcase/views/textarea.js
@@ -4,9 +4,16 @@ import 'view-units/unit-md'
 
 export function textarea( state = "" ) {
 
-  const state1 = { "value": state, spellcheck: true, "chars": 100, "font-size": "1.4rem" }
+  const state1 = { "value": state, label: "Label", spellcheck: true, "chars": 100, "font-size": "1.4rem" }
   const state2 = { "value": state, label: "You can also type here", badge: false }
   const state3 = { "value": state.split( "" ).reverse().join( "" ), disabled: true, label: "Mirror above area" }
+  const state4 = {
+    "value": `Erste Zeile
+Ein Satz zum Teilselektieren
+    `,
+    disabled: true,
+    label: "Mirror above area"
+  }
 
   return html `
     <article class="textarea" hidden="false">
@@ -14,14 +21,20 @@ export function textarea( state = "" ) {
       <h1>Element unit-textarea</h1>
 
       <h2>Example</h2>
+
       <h3>value, chars:100, font-size:1.4rem, spellcheck: true</h3>
       <unit-textarea id="1" .state=${state1}></unit-textarea>
+
       <h3>"label":"You can also type here"</h3>
       <unit-textarea id="2" .state=${state2}></unit-textarea>
       <unit-textarea id="3" .state=${state3}></unit-textarea>
 
+      <h3>"label":"You can also type here"</h3>
+      <unit-textarea id="3" .state=${state4}></unit-textarea>
+
       <h2>Signature</h2>
       <unit-md .state=${{ raw: Unit.signature, theme:"tomorrow" }}></unit-md>
+
     </article>
   `
 
