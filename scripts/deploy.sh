71e79371442e766f74fb0de0703643f9626cc217
Marco <marco.egbring@epha.ch>
2019-09-06 12:58:56 +0200

    Upload Fresh Build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,25 @@ then
   aws --region eu-central-1 s3 --delete sync dist s3://epha.io/$view/ --exclude .DS_Store
 
   echo ""
-  paths="/$view/*"
+  paths="/$view/index.html"
+  echo "> Invalidating ${paths} - Please wait"
+  id=$(aws cloudfront create-invalidation --distribution-id $dist --paths $paths | grep Id | awk -F '"' '{print $4}')
+  aws cloudfront wait invalidation-completed --distribution-id $dist --id $id
+  osascript -e 'display notification "Invalidated showcase" with title "AWS" subtitle "Cloudfront" sound name "Submarine" '
+  open "https://epha.io/$view/"
+
+fi
+
+if [[ $1 == "release" ]]
+then
+  dist="ES8QILS3Z5OA1"
+
+  npm run build
+
+  aws --region eu-central-1 s3 --delete sync dist s3://epha.ch/$view/ --exclude .DS_Store
+
+  echo ""
+  paths="/$view/index.html"
   echo "> Invalidating ${paths} - Please wait"
   id=$(aws cloudfront create-invalidation --distribution-id $dist --paths $paths | grep Id | awk -F '"' '{print $4}')
   aws cloudfront wait invalidation-completed --distribution-id $dist --id $id
