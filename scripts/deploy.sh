56002e7dad149be96b0a0b92b7e9c7647064b6db
Marco <marco.egbring@epha.ch>
2019-09-07 00:21:55 +0200

    add release script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -49,7 +49,7 @@ then
   id=$(aws cloudfront create-invalidation --distribution-id $dist --paths $paths | grep Id | awk -F '"' '{print $4}')
   aws cloudfront wait invalidation-completed --distribution-id $dist --id $id
   osascript -e 'display notification "Invalidated showcase" with title "AWS" subtitle "Cloudfront" sound name "Submarine" '
-  open "https://epha.io/$view/"
+  open "https://epha.ch/$view/"
 
 fi
 
