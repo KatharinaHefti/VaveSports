5e84a3bebb00269a3ae986e5a2f354856a12b50d
Anastasiia Shevchuk <anastasiia.shevchuk@epha.ch>
2019-09-19 17:07:05 +0200

    Toggle unit and switch button

--- showcase/views/toggle.js
+++ showcase/views/toggle.js
@@ -1,14 +1,39 @@
 import { html, render } from 'lit-html'
-import Unit from 'view-units/unit-toggle/'
+import { styleMap } from 'lit-html/directives/style-map.js'
+import immu from 'object-path-immutable'
 
-export function toggle( state = "" ) {
+// Layz import units
+import unitToggle from 'view-units/unit-toggle/'
+import 'view-units/unit-md/'
+
+export function toggle( state = 0 ) {
 
   return html `
-    <article class="toggle" hidden="false">
+    <article hidden=false>
 
-      <h1>Toggle</h1>
+      <h1>Element unit-toggle</h1>
+
+      <h2>Examples</h2>
+
+      <h3>Toggle switch (on/off button)</h3>
+      <section>
+        <unit-toggle .state=${ {
+          id:"toggle1"
+        }}></unit-toggle>
+      </section>
+
+      <h3>Disabled toggle switch</h3>
+      <section>
+        <unit-toggle .state=${ {
+          id:"toggle2",
+          "className": "disabled",
+          "disabled": "disabled"
+        }}></unit-toggle>
+      </section>
+
+      <h2>Signature</h2>
+      <unit-md .state=${{ raw: unitToggle.signature, theme:"tomorrow" }}></unit-md>
 
     </article>
-
   `
 }
